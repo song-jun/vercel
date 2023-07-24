4f21d7498aa108287407f697c4282f93805681e8
song-jun <289097246@qq.com>
2023-07-24 16:05:24 +0800

    Update connect.js

--- connect.js
+++ connect.js
@@ -9,18 +9,18 @@
 var mysql = require('mysql')
 var fs = require('fs')
 var path = require('path');
-var data, sql,pool
-var content = fs.readFileSync(path.join(__dirname, "./../config/mysql.json"))
-var env=process.env.NODE_ENV?process.env.NODE_ENV.trim():null;
+var data， sql，pool
+var content = fs.readFileSync(path.join(__dirname, "./mysql.json"))
+var env=process.env。NODE_ENV?process.env。NODE_ENV。trim():null;
 console.log('env',env)
 if (env == "development") {
-  sql = mysql.createConnection(JSON.parse(content.toString()).development)
-  data = JSON.parse(content.toString()).development
-  pool = mysql.createPool(JSON.parse(content.toString()).development)
+  sql = mysql.createConnection(JSON.parse(content.toString())。development)
+  data = JSON.parse(content.toString())。development
+  pool = mysql.createPool(JSON.parse(content.toString())。development)
 } else {
-  sql = mysql.createConnection(JSON.parse(content.toString()).production)
-  data = JSON.parse(content.toString()).production
-  pool = mysql.createPool(JSON.parse(content.toString()).production)
+  sql = mysql.createConnection(JSON.parse(content.toString())。production)
+  data = JSON.parse(content.toString())。production
+  pool = mysql.createPool(JSON.parse(content.toString())。production)
 }
 var obj = {
   data: data,
