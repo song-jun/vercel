1191df68df261dc59fb717dcacef9b69c9a38a51
song-jun <289097246@qq.com>
2023-07-27 10:23:03 +0800

    qq

--- index.js
+++ index.js
@@ -8,27 +8,27 @@ app.get("/mytest", function (req, res) {
 });
 
 // http
-// let server = app.listen(1314, function () {
-//   let port = server.address().port;
-//   console.log(`应用实例，访问地址为 http://localhost:${port}`);
-// });
+let server = app.listen(1314, function () {
+  let port = server.address().port;
+  console.log(`应用实例，访问地址为 http://localhost:${port}`);
+});
 
 // https
 // 创建https服务器实例
-const https = require('https')
-let key = fs.readFileSync(`${__dirname}/ssl/test.com.key`, 'utf-8')
-let cert = fs.readFileSync(`${__dirname}/ssl/test.com.pem`, 'utf-8')
-// console.log('log',key,cert);
-const credentials = {
-  key: key,
-  cert: cert
-}
-const httpsServer = https.createServer(credentials, app)
+// const https = require('https')
+// let key = fs.readFileSync(`${__dirname}/ssl/test.com.key`, 'utf-8')
+// let cert = fs.readFileSync(`${__dirname}/ssl/test.com.pem`, 'utf-8')
+// // console.log('log',key,cert);
+// const credentials = {
+//   key: key,
+//   cert: cert
+// }
+// const httpsServer = https.createServer(credentials, app)
 
-// 设置https的访问端口号
-const SSLPORT = 1314
+// // 设置https的访问端口号
+// const SSLPORT = 3000
 
-// 启动服务器，监听对应的端口
-httpsServer.listen(SSLPORT, () => {
-  console.log(`HTTPS Server is running on: https://localhost:${SSLPORT}`)
-})
+// // 启动服务器，监听对应的端口
+// httpsServer.listen(SSLPORT, () => {
+//   console.log(`HTTPS Server is running on: https://localhost:${SSLPORT}`)
+// })
